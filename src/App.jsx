eacb6a586a6564bd42903720f7aed13d79b97044
Emerson Melo Martins <emerson25xd@gmail.com>
2022-06-16 12:35:52 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { Post } from "./components/Post";
+import { Header } from "./components/Header";
+
+import styles from "./App.module.css";
+import "./global.css";
+import { Sidebar } from "./components/Sidebar";
+
+export function App() {
+  return (
+    <div>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post author="Emerson Melo" content="Lorem" />
+          <Post author="Emerson Melo" content="Lorem" />
+          <Post author="Emerson Melo" content="Lorem" />
+        </main>
+      </div>
+    </div>
+  );
+}
