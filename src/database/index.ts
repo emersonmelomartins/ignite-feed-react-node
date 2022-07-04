ef736b81e279f0477bf860c615c1bb96154af04e
Emerson Melo Martins <emerson25xd@gmail.com>
2022-07-04 12:32:50 -0300

    typeorm migration script fix

--- src/database/index.ts
+++ src/database/index.ts
@@ -4,10 +4,10 @@ export const AppDataSource = new DataSource({
   type: "sqlite",
   database:
     process.env.NODE_ENV === "test"
-      ? "./src/database/test__ignite-feed.sql"
+      ? "./src/database/__test__ignite-feed.sql"
       : "./src/database/ignite-feed.sql",
-  // synchronize: true,
-  // logging: true,
+  synchronize: false,
+  logging: process.env.NODE_ENV === "test",
   entities: ["./src/modules/**/entities/*.ts"],
   migrations: ["./src/database/migrations/*.ts"],
 });
