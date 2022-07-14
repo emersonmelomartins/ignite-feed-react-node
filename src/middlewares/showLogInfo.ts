20fdaca56bb32c05364b6177d4afc93c375e652e
Emerson Melo Martins <emerson25xd@gmail.com>
2022-07-14 00:00:38 -0300

    Added console log with colors on log middleware

--- src/middlewares/showLogInfo.ts
+++ src/middlewares/showLogInfo.ts
@@ -7,6 +7,7 @@ export function showLogInfo(
   response: Response,
   next: NextFunction
 ) {
+  const CONSOLE_COLOR = "\x1b[36m%s\x1b[0m";
   const date = format(new Date(), "dd'-'MM'-'yyyy' 'HH':'mm':'ss");
 
   const { method, url } = request;
@@ -15,8 +16,10 @@ export function showLogInfo(
   const hrTime = process.hrtime();
   const duration = getDurationInMilliseconds(hrTime);
 
-  const log = `[${date}] ${method}:${url} -> ${statusCode} | ${duration.toFixed(2)}ms`;
-  console.log(log);
+  const log = `[${date}] ${method}:${url} -> ${statusCode} | ${duration.toFixed(
+    2
+  )}ms`;
+  console.log(CONSOLE_COLOR, log);
 
   return next();
 }
