22fa499187100adcd9044a25a1dc42d43196efb0
Emerson Melo Martins <emerson25xd@gmail.com>
2022-07-15 16:51:45 -0300

    Added initial server started message

--- server/src/shared/infra/http/routes/index.ts
+++ server/src/shared/infra/http/routes/index.ts
@@ -1,9 +1,13 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 import { postsRoutes } from "./posts.routes";
 import { sessionsRoutes } from "./sessions.routes";
 import { usersRoutes } from "./users.routes";
 
 export const routes = Router();
+
+routes.get("/", (request: Request, response: Response) => {
+  return response.send(`A aplicação está em execução, acesse a documentação em /swagger`)
+})
 
 routes.use("/users", usersRoutes);
 routes.use("/sessions", sessionsRoutes);
