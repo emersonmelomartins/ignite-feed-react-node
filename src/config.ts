a4678b57733142c283a3263dd82f099d41f5d32d
Emerson Melo Martins <emerson25xd@gmail.com>
2022-07-18 23:28:49 -0300

    Added prod api url

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 const setEnvironment = (env = "dev") =>
   ({
     dev: "http://localhost:3333",
-    prod: "http://google.com.br",
+    prod: "https://ignite-feed-note.herokuapp.com/",
   }[env]);
 
 export const environmentUrl = setEnvironment(import.meta.env.VITE_APP_ENV);
