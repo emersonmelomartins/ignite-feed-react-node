d5908405c9ce45a9cef1fb1713ac3c888b493028
Emerson Melo Martins <emerson25xd@gmail.com>
2022-07-19 16:38:21 -0300

    Added cloudinary storage implementation

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -1,8 +1,15 @@
 import { container } from "tsyringe";
+import { CloudinaryStorageProvider } from "./implementations/CloudinaryStorageProvider";
 import { LocalStorageProvider } from "./implementations/LocalStorageProvider";
 import { IStorageProvider } from "./IStorageProvider";
+import "dotenv/config";
+
+const storageProvider =
+  process.env.APP_ENVIRONMENT === "dev"
+    ? LocalStorageProvider
+    : CloudinaryStorageProvider;
 
 container.registerSingleton<IStorageProvider>(
-  "LocalStorageProvider",
-  LocalStorageProvider
+  "StorageProvider",
+  storageProvider
 );
